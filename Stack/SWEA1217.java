23eccd4b15d98e26f746df1664a41e40fd613ba1
csmoon1010 <hjmoon1010@gmail.com>
2020-12-28 11:32:59 +0900

    SWEA1217

---
+++ Stack/SWEA1217.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class SWEA1217
+{
+	public static void main(String args[]) throws Exception
+	{
+		System.setIn(new FileInputStream("Stack/res/input.txt"));
+		Scanner sc = new Scanner(System.in);
+
+		for(int test_case = 1; test_case <= 10; test_case++)
+		{
+			int order = sc.nextInt();
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			int answer = calcPower(n, m);
+			System.out.println("#" + order + " " + answer);
+		}
+	}
+	
+	public static int calcPower(int n, int m) {
+		if(m == 1)
+			return n;
+		else
+			return n * calcPower(n, m-1);
+	}
+}
