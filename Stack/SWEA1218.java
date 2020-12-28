910f40cab0a4353ecc5ebcec3cf4f408665e55a2
csmoon1010 <hjmoon1010@gmail.com>
2020-12-28 11:57:59 +0900

    SWEA1218

---
+++ Stack/SWEA1218.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.Stack;
+import java.util.HashMap;
+
+class SWEA1218
+{
+	public static void main(String args[]) throws Exception
+    {
+		System.setIn(new FileInputStream("Stack/res/input2.txt"));
+		Scanner sc = new Scanner(System.in);
+		char[] list1 = new char[] {'(', '[', '{', '<'};
+		char[] list2 = new char[] {')', ']', '}', '>'};
+		HashMap<Character, Integer> left = new HashMap<>();
+		HashMap<Character, Integer> right = new HashMap<>();
+		for(int i = 0; i < 4; i++)	left.put(list1[i], i);
+		for(int i = 0; i < 4; i++)	right.put(list2[i], i);
+		for(int test_case = 1; test_case <= 10; test_case++)
+		{
+			int answer = 1;
+			Stack<Character> stack = new Stack<>();
+			int size = sc.nextInt();
+			String s = sc.next();
+			for(int i = 0; i < size; i++) {
+				char target = s.charAt(i);
+				if(left.containsKey(target)) {
+					stack.push(target);
+				}
+				else if(right.containsKey(target)) {
+					if(right.get(target) == left.get(stack.peek())) {
+						stack.pop();
+					}else {
+						answer = 0;
+						break;
+					}
+				}
+			}
+			if(answer == 1 && !stack.empty())	answer = 0;
+			System.out.println("#" + test_case + " " + answer);
+		}
+	}
+}
